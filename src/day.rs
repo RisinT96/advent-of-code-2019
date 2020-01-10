8825c8c8c2bd3a01162d85d1a552e37dd9364287
Tal Risin <risint96@gmail.com>
2020-01-10 14:18:04 +0200

    Updated main, added day1 data and code skeleton

---
+++ src/day.rs
@@ -0,0 +1,6 @@
+use std::fs::File;
+use std::io::{BufReader, Lines};
+
+pub fn compute_day_1(data: Lines<BufReader<File>>) -> String {
+    return "FUCK".into();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,35 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader, Lines};
+
 use std::io;
 
-fn main() {
-    println!("Select day: ");
-    let mut line = String::new();
+mod day;
 
+
+
+fn main() {
+    
     loop {
-        line.clear();
+        println!("Select day: ");
+        let mut line = String::new();
         io::stdin()
             .read_line(&mut line)
             .expect("Failed to read line!");
 
         /* Check exit options */
-        match line.trim() {
+        let result = match line.trim() {
+            "1" => day::compute_day_1(load_data_file("inputs/1.txt")),
             "exit" | "q" => break,
-            "" => continue,
-            _ => (),
+            _ => continue,
         };
+
+        println!("{}",result)
     }
+}
+
+fn load_data_file(filename: &str) -> Lines<BufReader<File>> {
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    return reader.lines();
 }
