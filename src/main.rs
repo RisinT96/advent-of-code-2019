7a3a8e995afa7dffaeef4d003f723f2f5918ed9e
Tal Risin <risint96@gmail.com>
2020-01-10 13:42:44 +0200

    Updated main structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Select day: ");
+    let mut line = String::new();
+
+    loop {
+        line.clear();
+        io::stdin()
+            .read_line(&mut line)
+            .expect("Failed to read line!");
+
+        /* Check exit options */
+        match line.trim() {
+            "exit" | "q" => break,
+            "" => continue,
+            _ => (),
+        };
+    }
 }
