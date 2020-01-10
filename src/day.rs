63d5a66432fbc5963e72f403a4d651851c092cb2
Tal Risin <risint96@gmail.com>
2020-01-10 14:39:16 +0200

    Solved part 1

--- src/day.rs
+++ src/day.rs
@@ -2,5 +2,13 @@ use std::fs::File;
 use std::io::{BufReader, Lines};
 
 pub fn compute_day_1(data: Lines<BufReader<File>>) -> String {
-    return "FUCK".into();
+    let mut fuel: u32 = 0;
+
+    for module in data {
+        let mass: u32 = module.unwrap().parse().unwrap();
+
+        fuel += (mass / 3) - 2;
+    }
+
+    return fuel.to_string();
 }
